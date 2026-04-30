ba2c580bf273f2ee52718194412f72308453c093
Paul <145344090+DrPAHAN@users.noreply.github.com>
2026-04-30 14:31:58 +0700

    Add files via upload

--- Task2/t3/3_v1.cpp
+++ Task2/t3/3_v1.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 #include <chrono>
 #include <omp.h>
 
 int main(int argc, char** argv) {
     if (argc < 4) {
-        std::cerr << "Usage: ./simple_iter_v1 N threads num_iterations\n";
+        std::cerr << "Usage: ./simple_iter_v1 N threads iterations\n";
         return 1;
     }
 
     long long N = std::stoll(argv[1]);
     int threads = std::stoi(argv[2]);
-    long long num_iter = std::stoll(argv[3]);
+    long long iterations = std::stoll(argv[3]);
 
     omp_set_num_threads(threads);
 
@@ -20,38 +19,35 @@ int main(int argc, char** argv) {
     std::vector<double> x_new(N);
     double tau = 0.5 / N;
 
-    auto t_start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
 
-    for (long long it = 0; it < num_iter; ++it) {
+    for (long long it = 0; it < iterations; ++it) {
         double sum_all = 0.0;
 
-        // 1. Суммируем все элементы x
 #pragma omp parallel for reduction(+:sum_all)
         for (long long i = 0; i < N; ++i) {
             sum_all += x[i];
         }
 
-        // 2. Вычисляем новое приближение
 #pragma omp parallel for
         for (long long i = 0; i < N; ++i) {
-            double Ax = sum_all + x[i];                 // A*x (диагональ 2, остальные 1)
+            double Ax = sum_all + x[i];
             double res = Ax - (N + 1.0);
             x_new[i] = x[i] - tau * res;
         }
 
-        // 3. Копируем новое приближение обратно
 #pragma omp parallel for
         for (long long i = 0; i < N; ++i) {
             x[i] = x_new[i];
         }
     }
 
-    auto t_end = std::chrono::high_resolution_clock::now();
-    double time = std::chrono::duration<double>(t_end - t_start).count();
+    auto end = std::chrono::high_resolution_clock::now();
+    double time = std::chrono::duration<double>(end - start).count();
 
-    std::cout << "V1 (separate parallel for) | N=" << N 
+    std::cout << "V1 | N=" << N 
               << " | threads=" << threads 
-              << " | iterations=" << num_iter 
+              << " | iterations=" << iterations 
               << " | time=" << time << " s" << std::endl;
 
     return 0;
