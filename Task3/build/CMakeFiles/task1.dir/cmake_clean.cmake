f9a85e49b76d198d9a4d872d98fa158f463b4f5c
Paul <145344090+DrPAHAN@users.noreply.github.com>
2026-05-14 12:23:47 +0700

    Add files via upload

---
+++ Task3/build/CMakeFiles/task1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task1.dir/task1_matrix_vector.cpp.o"
+  "CMakeFiles/task1.dir/task1_matrix_vector.cpp.o.d"
+  "bin/task1"
+  "bin/task1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Task3/build/CMakeFiles/task2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task2.dir/task2_client_server.cpp.o"
+  "CMakeFiles/task2.dir/task2_client_server.cpp.o.d"
+  "bin/task2"
+  "bin/task2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
